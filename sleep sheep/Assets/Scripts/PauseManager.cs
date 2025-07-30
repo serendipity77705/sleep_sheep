b12d4455ede87a285e3d24f31da71796d2206004
Malk Daboor <malk.daboor@dal.ca>
2025-07-29 22:51:53 -0300

    pase scene present but needs work

---
+++ sleep sheep/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool isPausedScene;
+    public GameObject pauseMenuUI;
+
+    void Start()
+    {
+        // Detect whether we're in the PauseScene
+        isPausedScene = SceneManager.GetActiveScene().name == "PauseScene";
+        
+        if (!isPausedScene)
+            Time.timeScale = 1f;  // Normal speed
+        else
+            Time.timeScale = 0f;  // Game is paused
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isPausedScene)
+            {
+                // Resume game: go back to gameplay scene
+                ResumeGame();
+            }
+            else
+            {
+                // Pause game: go to pause scene
+                PauseGame();
+            }
+        }
+    }
+    
+    public void ResumeGame()
+    {
+        SceneManager.LoadScene("CollectionScene");
+    }
+
+    public void PauseGame()
+    {
+        SceneManager.LoadScene("PauseScene");
+    }
+}
